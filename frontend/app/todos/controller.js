0b8d2a3283db73550f786dcd33117ed37753e266
Tim Rourke <tim@timrourke.com>
2018-09-24 17:34:12 -0500

    Extract frontend into own repository

--- frontend/app/todos/controller.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-import EmberObject from '@ember/object';
-
-export default Controller.extend({
-  newTodo: EmberObject.create({
-    title: '',
-    description: '',
-  }),
-});
